2368f64265105e7763145ea7127b381b2c49b229
blessedjasonmwanza <mwanzabj@gmail.com>
2022-02-02 12:51:08 +0200

    Add tables library

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,5 +1,80 @@
 import React from 'react';
+import { MDBDataTable } from 'mdbreact';
+import './css/Missions.css';
 
 export default function Missions() {
-  return <div>Missions</div>;
+  const data = {
+    columns: [
+      {
+        label: 'Name',
+        field: 'name',
+        sort: 'asc',
+        width: 150
+      },
+      {
+        label: 'Position',
+        field: 'position',
+        sort: 'asc',
+        width: 270
+      },
+      {
+        label: 'Office',
+        field: 'office',
+        sort: 'asc',
+        width: 200
+      },
+      {
+        label: 'Age',
+        field: 'age',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'Start date',
+        field: 'date',
+        sort: 'asc',
+        width: 150
+      },
+      {
+        label: 'Salary',
+        field: 'salary',
+        sort: 'asc',
+        width: 100
+      }
+    ],
+    rows: [
+      {
+        name: 'Tiger Nixon',
+        position: 'System Architect',
+        office: 'Edinburgh',
+        age: '61',
+        date: '2011/04/25',
+        salary: '$320'
+      },
+      {
+        name: 'Garrett Winters',
+        position: 'Accountant',
+        office: 'Tokyo',
+        age: '63',
+        date: '2011/07/25',
+        salary: '$170'
+      },
+      {
+        name: 'Ashton Cox',
+        position: 'Junior Technical Author',
+        office: 'San Francisco',
+        age: '66',
+        date: '2009/01/12',
+        salary: '$86'
+      },
+    ]
+  };
+  return (
+    <MDBDataTable
+      striped
+      bordered
+      hover
+      data={data}
+    />
+  );
 }

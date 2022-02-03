0f58f0f0a5b354748c37be55861aee10c2e18d24
blessedjasonmwanza <mwanzabj@gmail.com>
2022-02-03 20:11:40 +0200

    Use filter to display joined missions

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,13 +3,14 @@ import './css/Profile.css'
 
 export default function Profile() {
   const missions = JSON.parse(localStorage.getItem('missions')) || [];
+  const joinedMissions = missions.filter((mission) => mission.status === 'booked');
   return (
     <div className='profile'>
       <section>
         <h2>My Missions</h2>
         <ul>
           {
-            missions.map(mission => mission.status === "booked" ? <li>{mission.mission_name}</li> : null)
+            joinedMissions.map(mission => <li>{mission.mission_name}</li>)
           }
         </ul>
       </section>

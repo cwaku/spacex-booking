992297b800b7dc65ab7d951c826759542491c9c3
blessedjasonmwanza <mwanzabj@gmail.com>
2022-02-05 00:36:18 +0200

    Fix linter errors in test files

--- src/components/_tests_/Profile.test.js
+++ src/components/_tests_/Profile.test.js
@@ -1,20 +1,18 @@
 import '@testing-library/jest-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { render } from '@testing-library/react';import store from '../../redux/store/configureStore';
+import { render } from '@testing-library/react'; import store from '../../redux/store/configureStore';
 import Profile from '../Profile';
 
-describe("The Profile component", () => {
-  test("rendering of the Profile component", () => {
+describe('The Profile component', () => {
+  test('rendering of the Profile component', () => {
     const { getByText } = render(
       <Provider store={store}>
         <BrowserRouter>
           <Profile />
         </BrowserRouter>
-      </Provider>
+      </Provider>,
     );
-    // eslint-disable-next-line testing-library/prefer-screen-queries
     expect(getByText('My Missions')).toBeInTheDocument();
   });
 });
-
--- src/components/_tests_/Rockets.test.js
+++ src/components/_tests_/Rockets.test.js
@@ -1,11 +1,11 @@
 import '@testing-library/jest-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { render } from '@testing-library/react';import store from '../../redux/store/configureStore';
+import { render } from '@testing-library/react'; import store from '../../redux/store/configureStore';
 import RocketItem from '../rockets/RocketItem';
 
-describe("the Rockets component", () => {
-  test("rendering of the rockets component", () => {
+describe('the Rockets component', () => {
+  test('rendering of the rockets component', () => {
     const { getByText } = render(
       <Provider store={store}>
         <BrowserRouter>
@@ -15,13 +15,11 @@ describe("the Rockets component", () => {
             description: 'The Falcon 9 is a two-stage rocket developed by SpaceX and later partially funded by NASA. It is the second-to-last stage of the Falcon rocket family. The Falcon 9 is the second-stage booster to be developed for the Falcon Heavy, and the first to be developed for the Falcon 9.',
             flickr_images: 'https://farm1.staticflickr.com/835/29181844189_c7d9f9c8c3_o.jpg',
             reserved: false,
-          }} />
+          }}
+          />
         </BrowserRouter>
-      </Provider>
+      </Provider>,
     );
-    // eslint-disable-next-line testing-library/prefer-screen-queries
     expect(getByText('Falcon 9')).toBeInTheDocument();
   });
 });
-
-
